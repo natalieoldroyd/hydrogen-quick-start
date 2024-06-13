1c01417e8e612ad3958068f68b0567ab1afe7405
Natalie Oldroyd <natalie.oldroyd@shopify.com>
2024-06-13 12:12:27 -0230

    remove favicon

--- app/root.jsx
+++ app/root.jsx
@@ -10,7 +10,7 @@ import {
   ScrollRestoration,
   isRouteErrorResponse,
 } from '@remix-run/react';
-import favicon from '~/assets/favicon.svg';
+// import favicon from '~/assets/favicon.svg';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
 import {PageLayout} from '~/components/PageLayout';
@@ -46,7 +46,7 @@ export function links() {
       rel: 'preconnect',
       href: 'https://shop.app',
     },
-    {rel: 'icon', type: 'image/svg+xml', href: favicon},
+    // {rel: 'icon', type: 'image/svg+xml', href: favicon},
   ];
 }
 
